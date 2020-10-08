6222252e0a99dc39f2958a68511fd4c48cc6baf4
921-14\AU <aa070312@au.edu.tw>
2020-10-08 11:51:06 +0800

    2020/10/08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,18 +5,16 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   // 屬性(Property): 教學單位陣列
-  title = '真理大學';  // 屬性：字串
-  units = [            // 屬性：陣列
-    '人文學院',
-    '觀光休閒與運動學院',
-    '管理學院',
-    '財經學院',
-    '資訊與商業智慧學院',
-    '通識教育中心',
-    '體育教育中心'
-  ];
+  title = '關於我 AboutMe';  // 屬性：字串
+  author = '莊立婷 Nina';
+  myfile = 'me.JPG';
+  petName = '多多 Dodo';
+  petfile = 'dodo.JPG';
+  nickname = ' aka 小多子';
+
 
   // 方法(Method): 選取項目
   itemSelected(e: any) {

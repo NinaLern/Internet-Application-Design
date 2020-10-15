cb26e5ea9325d18e89802ff7c29b5b786f028946
921-14\AU <aa070312@au.edu.tw>
2020-10-15 12:02:26 +0800

    2020/10/15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,12 +9,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   // 屬性(Property): 教學單位陣列
   title = '關於我 AboutMe';  // 屬性：字串
-  author = '莊立婷 Nina';
-  myfile = 'me.JPG';
-  petName = '多多 Dodo';
-  petfile = 'dodo.JPG';
-  nickname = ' aka 小多子';
+  menu = ['基本資料', '我的寶貝', '其他', '聯絡我'];
+  // author = '莊立婷 Nina';
+  logo = '/assets/images/me.JPG';
+  // petName = '多多 Dodo';
+  // petfile = 'dodo.JPG';
+  // nickname = ' aka 小多子';
+  mycss = 'text-danger';
 
+  myEvent(event) {
+    console.log(event);
+  }
+
+  changeColor() {
+    if (this.mycss === 'text-danger') {
+      this.mycss = 'text-primary';
+    } else {
+      this.mycss = 'text-danger';
+    }
+  }
 
   // 方法(Method): 選取項目
   itemSelected(e: any) {
